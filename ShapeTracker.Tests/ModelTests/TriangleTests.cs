b7d8faa17760fa7ea202309fbbb9d65dfba0d9f6
SimphiweMbokazi <simphiwem380@gmail.com>
2023-12-09 19:49:21 +0200

    passed triangle constructor tests

---
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShapeTracker.Models;
+
+namespace ShapeTracker.Tests
+{
+  [TestClass]
+  public class TriangleTests
+  {
+    // Tests will go here!
+    [TestMethod]
+    public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
+    {
+      Triangle newTriangle = new Triangle();
+      Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
+    }
+  }
+}
